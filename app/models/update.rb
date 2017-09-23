bf4531a1b369211f63d610a3892e02b7aa7f2e7c
Michael McDonald <michaelrmcd@gmail.com>
2017-09-22 21:03:17 -0700

    Finish approve/deny for staff

--- app/models/update.rb
+++ app/models/update.rb
@@ -3,6 +3,8 @@ class Update < ApplicationRecord
   belongs_to :user
   after_initialize :set_defaults, unless: :persisted?
 
+  validates :title, :description, presence: true
+
   def set_defaults
     self.approval ||= false
   end
